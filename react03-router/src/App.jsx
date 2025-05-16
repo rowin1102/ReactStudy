3b56d3853396d9238bf5cb376f68b331423497cc
rowin1102 <rowin1102@naver.com>
2025-05-16 11:27:29 +0900

    2025-05-16

---
+++ react03-router/src/App.jsx
@@ -0,0 +1,156 @@
+/* react-router-dom으로부터 임포트한 컴포넌트와 관련 훅 */
+import {Routes, Route, Link, NavLink} from 'react-router-dom';
+import {Outlet, useLocation, useSearchParams} from 'react-router-dom';
+
+
+const TopNavi = () => {
+  /* NavLink 컴포넌트는 a태그와 같이 하이퍼링크를 제공한다.
+    단, a태그에 preventDefault()가 적용된 형태로 화면의 깜빡임없이 페이지 이동을
+    할 수 있다. 또한 링크를 클릭했을때 active라는 클래스 속성을 자동으로 추가해준다. */
+  return (<>
+    <nav>
+      <NavLink to={'/'}>HOME</NavLink> &nbsp;
+      <NavLink to={'/intro'}>인트로</NavLink> &nbsp;
+      <NavLink to={'/intro/router'}>Router관련Hook</NavLink> &nbsp;
+      <NavLink to={'/xyz'}>잘못된URL</NavLink> &nbsp;
+
+      {/* a태그를 사용하는 경우 화면의 깜박임이 있으므로 이벤트 객체를 통해
+        반드시 preventDefault()함수를 사용해야 한다. */}
+      <a href="/aTag" onClick={(e) => {e.preventDefault();}}>A태그</a> &nbsp;
+      {/* Link 컴포넌트는 NavLink와 기능은 동일하지만, active 속성값이 부여되지 않는다. */}
+      <Link to='/linkTag'>Link컴포넌트</Link>
+    </nav>
+  </>);
+}
+
+/* Outlet 컴포넌트 : 웹사이트 제작시 공통으로 사용되는 레이아웃에서 특정 요청에 따른
+    내용만 변경해야 할때 사용한다. */
+const CommonLayout = () => {
+  return (<>
+    <header style={{background:'lightgray', padding:'10px'}}>
+      Outlet 컴포넌트 알아보기
+    </header>
+    <article>
+      {/* 각 페이지의 컴포넌트가 보여지는 부분에 설정한다. */}
+      <Outlet />
+    </article>
+    <footer style={{background:'lightgray', padding:'10px'}}>
+      공통 레이아웃
+    </footer>
+  </>);
+}
+
+const Home = () => {
+  return (<>
+    <h2>React Home</h2>
+    <p>
+      React Router에 대해 학습합니다.
+    </p>
+  </>);
+}
+
+/* localhost/intro 경로가 요청될때 Outlet 컴포넌트 위치에 삽입되어 렌더링된다.
+  이 부분은 App 컴포넌트에 라우팅처리 되어있다. */
+const LayoutIndex = () => {
+  return (<>
+    <h2>레이아웃 인덱스 페이지</h2>
+    <ul>
+      <li>Outlet 컴포넌트 위치에 출력됩니다.</li>
+      <li>Route 설정시 index로 지정합니다.</li>
+    </ul>
+  </>);
+}
+
+/* 설정된 경로 외 잘못된 경로를 요청했을때 렌더링되는 컴포넌트.
+  백앤드에서는 이런 경우 404에러가 발생하게 된다. */
+const NotFound = () => {
+  return (<>
+    <h2>Not Found</h2>
+    <p>
+      페이지를 찾을 수 없습니다. <br />
+      <Link to='/'>Home</Link>
+    </p>
+  </>);
+}
+
+/* localhost/intro/router 경로가 요청되었을때 Outlet 컴포넌트 부분에 삽입되어 렌더링된다. */
+const RouterHooks = () => {
+  const location = useLocation();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const mode = searchParams.get('mode');
+  const pageNum = searchParams.get('pageNum');
+
+  const changeMode = () => {
+    const nextMode = (mode === 'list') ? 'view' : 'list';
+    setSearchParams({
+      mode : nextMode,
+      pageNum
+    });
+  }
+  
+  const nextPage = () => {
+    let pageTemp = (pageNum === null || isNaN(pageNum)) ? 1 : parseInt(pageNum) + 1;
+    if(pageTemp === 11) {
+      pageTemp = 10;
+      window.alert('마지막 페이지입니다.');
+    }
+    setSearchParams({
+      mode,
+      pageNum : pageTemp
+    });
+  }
+  
+  const prevPage = () => {
+    let pageTemp = (pageNum === null || isNaN(pageNum)) ? 1 : parseInt(pageNum) - 1;
+    if(pageTemp === 0) {
+      pageTemp = 1;
+      window.alert('첫 번째 페이지입니다.');
+    }
+    setSearchParams({
+      mode,
+      pageNum : pageTemp
+    });
+  }
+
+  return (<>
+    <h2>라우터 관련 Hook</h2>
+    <div>
+      <ul>
+        <li>URL : {location.pathname}</li>
+        <li>쿼리스트링 : {location.search}</li>
+        <li>mode : {mode}</li>
+        <li>detail : {pageNum}</li>
+      </ul>
+
+      <button onClick={changeMode}>mode변경</button>
+      <button onClick={prevPage}>이전Page</button>
+      <button onClick={nextPage}>다음Page</button>
+    </div>
+  </>);
+}
+
+function App() {
+  return (<>
+    {/* 라우팅 처리가 필요없는 컴포넌트는 전체페이지에서 공통으로 렌더링되는 네비게이션을
+      주로 사용한다. */}
+    <TopNavi />
+    {/* 라우팅 처리가 필요한 컴포넌트는 아래와 같이 path, element라는 속성을 통해 
+      경로와 렌더링 할 컴포넌트를 지정한다. */}
+    <Routes>
+      <Route path='/' element={<Home />} />
+      {/* 하위 경로가 필요한 경우에는 '중첩라우팅'을 사용한다. */}
+      <Route path='/intro' element={<CommonLayout />}>
+        {/* '/intro'로 요청이 들어오면 이 컴포넌트를 렌더링 */}
+        <Route index element={<LayoutIndex />} />
+        {/* '/intro/router'와 같이 하위경로로 요청이 들어오면 RouterHooks컴포넌트를
+          렌더링한다. */}
+        <Route path='router' element={<RouterHooks />} />
+      </Route>
+      {/* 지정되지 않은 모든 경로에 대해서는 404 처리 */}
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  </>); 
+}
+
+export default App
