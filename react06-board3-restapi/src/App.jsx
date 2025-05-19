bb88d9c48de9cdad61d8cce68737231ae73c5be7
rowin1102 <rowin1102@naver.com>
2025-05-19 16:01:20 +0900

    2025-05-19

--- react06-board3-restapi/src/App.jsx
+++ react06-board3-restapi/src/App.jsx
@@ -10,6 +10,8 @@ function App() {
     <Routes>
       <Route path='/' element={<List />} />
       <Route path='/list' element={<List />} />
+      {/* 중첩라우팅으로 게시물의 일련번호가 하위경로 형태로 추가된다.
+        이것을 useParams 훅을 통해 읽어올 수 있다. */}
       <Route path='/view'>
         <Route path=':idx' element={<View />} />
       </Route>
--- react06-board3-restapi/src/components/board/View.jsx
+++ react06-board3-restapi/src/components/board/View.jsx
@@ -1,15 +1,41 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
-function View() {
+function View(props) {
+  // 중첩된 라우팅에서 일련번호를 읽어오기 위한 훅
+  let params = useParams();
+  console.log('idx', params.idx);
+
+  // 열람 API는 JSON 객체이므로 빈 객체를 초기값으로 지정
+  let [boardData, setBoardData] = useState({});
+  // API 요청 주소
+  let requestUrl = "http://nakja.co.kr/APIs/php7/boardViewJSON.php";
+  // 파라미터(쿼리스트링)
+  let parameter = "tname=nboard_news&idx=" + params.idx + "&apikey=c827aaa03d6375722014cbaa333416f2";
+
+  // 1차 렌더링 후 열람API 요청
+  useEffect(function() {
+    fetch(requestUrl + "?" + parameter)
+      .then((result) => {
+        return result.json();
+      })
+      .then((json) => {
+        console.log(json);
+        // 스테이트 변경 및 리렌더링
+        setBoardData(json);
+      });
+
+    return () => {
+      console.log('useEffect실행 => 컴포넌트 언마운트');
+    }
+  }, []);
+
   return (<>
     <header>
       <h2>게시판 - 읽기</h2>
     </header>
 
     <nav>
-      {/* <a href="/list">목록</a>&nbsp;
-      <a href="/edit">수정</a>&nbsp;
-      <a href="/delete">삭제</a> */}
       <Link to='/list'>목록</Link> &nbsp;
       <Link to='/edit'>수정</Link> &nbsp;
       <Link to='/delete'>삭제</Link>
@@ -18,25 +44,30 @@ function View() {
     <article>
       <table id="boardTable">
         <colgroup>
-          <col width="30%" />
+          <col width="20%" />
           <col width="*" />
         </colgroup>
         <tbody>
           <tr>
             <td>작성자</td>
-            <td>성유겸</td>
+            {/* 내용 출력은 JSON객체이므로 각 Key를 통해 접근하면 된다. */}
+            <td>{boardData.name}</td>
           </tr>
           <tr>
             <td>제목</td>
-            <td>오늘은 React공부하는 날</td>
+            <td>{boardData.subject}</td>
           </tr>
           <tr>
             <td>날짜</td>
-            <td>2023-05-05</td>
+            <td>{boardData.regdate}</td>
           </tr>
           <tr>
             <td>내용</td>
-            <td>열심히 해봅시다<br />열공합시다</td>
+            {/* HTML 태그가 그대로 출력됨. React는 보안적인 문제로 태그를 화면에
+              그대로 출력하는 것이 디폴트 설정이다. */}
+            {/* <td>{boardData.content}</td> */}
+            {/* 마크업이 적용된 상태로 출력됨 */}
+            <td dangerouslySetInnerHTML={{__html: boardData.content}}></td>
           </tr>
         </tbody>
       </table>
