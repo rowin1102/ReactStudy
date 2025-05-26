a64c6ccace9163f0432ec87166043479b4aad153
rowin1102 <rowin1102@naver.com>
2025-05-26 12:17:04 +0900

    2025-05-26

--- react14-realtime/src/App.jsx
+++ react14-realtime/src/App.jsx
@@ -12,6 +12,9 @@ export default function App() {
         <Route path="/" element={<RealtimeCRUD/>} />
         <Route path="/crud" element={<RealtimeCRUD/>} />
         <Route path="/listener" element={<Listener/>} />
+        {/* 실시간 채팅은 2단계로 라우팅 처리가 되어있음.
+          첫 번째 화면은 대화방, 대화명 입력을 위한 입력상자가 있음.
+          2개의 정보를 입력 후 팝업창으로 채팅 대화창(두번째화면)을 띄우게 됨. */}
         <Route path="/chat">
           <Route index element={<ChatStart/>} />
           <Route path="talk" element={<ChatMessage/>} />
